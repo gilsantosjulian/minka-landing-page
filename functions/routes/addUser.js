53598426fcd8d66419d4d94814d05e9e98fd2ae5
Julian <julian@minka.io>
2019-04-24 21:03:06 -0500

    feat(Layer.jsx/adUser.js/sender.js) Add invisible reRaptcha. Send confirmation email

--- functions/routes/addUser.js
+++ functions/routes/addUser.js
@@ -1,19 +1,45 @@
 const firebase = require('firebase-admin');
+const { isHumanOrBot } = require('../utils/isHumanOrBot');
+const { sender } = require('../utils/sender');
 
-exports.addUser = function(req, res) {
+const imgPath = 'https://firebasestorage.googleapis.com/v0/b/minka-web.appspot.com/o/mail.png?alt=media&token=30729b35-50b3-49cd-bde9-9b4338077e55';
+const subject = 'Minka Hackathon'
+const message = `<img src="${imgPath}" alt="Confirmation - Minka Hackathon" >`;
+
+exports.addUser = function (req, res) {
   const body = Object.assign(
-    req.body,
-    { createAt: firebase.firestore.FieldValue.serverTimestamp() }
+    req.body, {
+      createAt: firebase.firestore.FieldValue.serverTimestamp()
+    }
   );
-  
-  firebase
-  .firestore()
-  .collection('participants')
-  .add(body)
-  .then(() => {
-    res.send(true)
-  })
-  .catch((error) => {
-    res.send(error.message)
-  });
+
+  isHumanOrBot(body.token)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        delete body.token;
+        firebase
+          .firestore()
+          .collection('participants')
+          .add(body)
+          .then(() => {
+            return;
+          })
+          .catch((error) => {
+            res.send(error.message)
+          });
+        return;
+      }
+      throw Error('Is not a human');
+    })
+    .then(() => {
+      return sender({
+        ...body,
+        to: body.email,
+        message: message,
+        subject: subject,
+      });
+    })
+    .then(() => res.send(true))
+    .catch((error) => res.send(error.message))
 };
